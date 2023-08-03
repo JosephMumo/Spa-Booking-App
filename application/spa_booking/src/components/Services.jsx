dd6bcbe29c52b5a3c000ac8060b0d44232a9ed67
JosephMumo <josephmumo25@gmail.com>
2023-08-03 19:27:55 +0300

    adding the services section

--- application/spa_booking/src/components/Services.jsx
+++ application/spa_booking/src/components/Services.jsx
@@ -40,14 +40,14 @@ function Services() {
     
     return (
         <div className='p-8  text-center text-white relative bg-fixed xl:h-screen' style={styles}>
-            <h1 className='mt-4 text-3xl font-bold'>OUR SERVICES</h1>
+            <h1 className='mt-6 text-3xl font-bold'>OUR SERVICES</h1>
             <div className='w-full grid place-items-center text-custom'>
                 <PiDotsThreeOutlineBold size="30px" />
             </div>
             <p className='my-3'> Experience expert haircuts, beard trims, shaves, and styling by skilled professionals.</p>
             <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-5 place-items-center'>
                 { Data.map(item => {
-                    return (<div className='text-center h-auto w-68 gap-1 rounded-sm bg-black outline-custom place-items-center p-3 py-5 mt-3'
+                    return (<div className='text-center h-auto w-68 gap-1 rounded-sm  outline-custom place-items-center p-3 py-5 mt-3 hover:border border-custom'
                         key={item.id}>
                         <div className='w-full flex justify-center'>
                             <img src={item.imgUrl} alt='1' />
@@ -57,7 +57,7 @@ function Services() {
                         <br />
                         <p>{item.description}</p>
                         <br />
-                        <p className='text-xl font-bold'>more</p>
+                        <p className='text-xl font-bold'>.</p>
                         <br />
                     </div>)
                 })}
